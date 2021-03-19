460e5f5ca30da99d3f1e9afe54d213bb62ef227e
Bilal Saeed <bilalsaeed232@gmail.com>
2021-03-19 23:24:38 +0100

    THE PROBLEM: telescoping constructor is an antipattern, we cannot tell by looking at constructor what those properties mean

---
+++ Pizza.js
@@ -0,0 +1,10 @@
+class Pizza {
+  constructor(size, pepperoni, cheese, bacon) {
+    this.size = size;
+    this.pepperoni = pepperoni;
+    this.cheese = cheese;
+    this.bacon = bacon;
+  }
+}
+
+module.exports = Pizza;
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 //entry point file...
+const Pizza = require("./Pizza");
+
+const smallPizza = new Pizza("small", true, true, false); // <---- these parameters doesn't give any information about the property
+const largePizza = new Pizza("large", false, false, true);
+
+console.log(smallPizza);
+console.log(largePizza);
