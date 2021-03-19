215600c754c2427057471d0f2d1d91af182eb4d9
Bilal Saeed <bilalsaeed232@gmail.com>
2021-03-19 20:53:26 +0100

    THE PROBLEM: in index.js file the logs count and actual logs are not the same

---
+++ Logger.js
@@ -0,0 +1,17 @@
+class Logger {
+  constructor() {
+    this.logs = [];
+  }
+
+  get count() {
+    return this.logs.length;
+  }
+
+  log(message) {
+    const timestamp = new Date().toISOString();
+    this.logs.push({ timestamp, message });
+    console.log(`${timestamp} - ${message}`);
+  }
+}
+
+module.exports = Logger;
--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-//entry point file...
+const Logger = require("./Logger");
+const Shopper = require("./Shopper");
+const Store = require("./Store");
+
+const logger = new Logger();
+
+const alex = new Shopper("alex", 350);
+
+const bikeShop = new Store("Chain gang", [
+  {
+    item: "Mountain bike",
+    qty: 20,
+    price: 250,
+  },
+  {
+    item: "Trekking bike",
+    qty: 80,
+    price: 100,
+  },
+]);
+
+logger.log("Startin app...");
+
+logger.log("Finishing config...");
+
+//check how many logs this class is storing so far
+console.log(`${logger.count} logs total`);
+
+//iterate over all the logs and check them
+logger.logs.map((log) => console.log(log));
