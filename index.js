ff5ef8c7e0bb37b6ce7a469c43c28c358f6af6f5
Bilal Saeed <bilalsaeed232@gmail.com>
2021-03-20 22:03:00 +0100

    THE PROBLEM: Instead of letting one object deals with the request, let it pass through multiple objects and chain it, like in our case we will make a Storage class for every part of inventory

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 //entry point file...
+const Store = require("./Store");
+const inventory = require("./inventory");
+
+const skiShop = new Store("Super Skis", inventory.backroom); // <==== we are just looking into one particular area only
+const searchItem = "ski rack";
+
+let results = skiShop.find(searchItem);
+console.log(results);
