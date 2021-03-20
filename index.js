f17d3da41609c89a94c3b167f994e99176d85c5f
Bilal Saeed <bilalsaeed232@gmail.com>
2021-03-20 20:22:56 +0100

    THE PROBLEM: what if want to treat objects and children like tree do, with branches and leafs, we will use composite pattern

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const CatalogItem = require("./CatalogItem");
 //entry point file...
+let boots = new CatalogItem("Leather Boots", 33.9);
+let sneakers = new CatalogItem("Sneakers", 22.6);
+
+console.log(`Boots total: ${boots.total}`);
+
+boots.print();
+sneakers.print();
