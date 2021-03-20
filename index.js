e215b5ae1d53c3ce35d322ee97bdd5b307ea96f2
Bilal Saeed <bilalsaeed232@gmail.com>
2021-03-20 19:48:40 +0100

    THE PROBLEM: instead of working directly with the expensive object, we use a proxy to get access to the part of object we are interested in (fs module in this case)

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 //entry point file...
+const path = require("path");
+const fs = require("fs"); // <==== expensive object
+
+let txtFile = path.join(__dirname, "Readme.txt");
+let mdFile = path.join(__dirname, "Readme.md");
+
+let result = (error, content) => {
+  if (error) {
+    console.log("\x07");
+    console.error(error);
+    process.exit(0);
+  }
+
+  console.log("reading file...");
+  console.log(content);
+};
+
+fs.readFile(txtFile, "UTF-8", result);
+fs.readFile(mdFile, "UTF-8", result);
