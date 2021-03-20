db57c7de75d3c6a0f77b37179b59355a34aee1b2
Bilal Saeed <bilalsaeed232@gmail.com>
2021-03-20 20:30:26 +0100

    THE SOLUTION: we created another class CatalogGroup which can add work with either CatalogItem or another CatalogGroup, we have shown how shopping carts can use this pattern

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+const CatalogGroup = require("./CatalogGroup");
 const CatalogItem = require("./CatalogItem");
 //entry point file...
 let boots = new CatalogItem("Leather Boots", 33.9);
 let sneakers = new CatalogItem("Sneakers", 22.6);
 
-console.log(`Boots total: ${boots.total}`);
+//use a group of items
+let shoes = new CatalogGroup("All Shoes", [boots, sneakers]);
+let fruites = new CatalogGroup("All Fruits", [
+  new CatalogItem("Apples", 10.4),
+  new CatalogItem("Bananas", 12.1),
+]);
 
-boots.print();
-sneakers.print();
+let shoppingCart = new CatalogGroup("Shopping Cart", [shoes, fruites]); //Note: how easy it is to work with either a single object or collection
+
+shoppingCart.print();
+console.log(`Total: ${shoppingCart.total}`);
