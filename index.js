68a3d1e06393c46d5dc81440cb252d746424fd67
Bilal Saeed <bilalsaeed232@gmail.com>
2021-03-20 18:34:58 +0100

    THE PROBLEM: localStorage doesn't exists on nodejs, it's a browser thing, so we need some adapter to have the same kind of functionality localstorage gives us in nodejs

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //entry point file...
+console.log("localStorage length:", localStorage.length);
+
+let uid = localStorage.getItem("user_id");
+
+console.log("user_id: ", uid);
+
+if (!uid) {
+  console.log("No user_id found, setting it...");
+  localStorage.setItem("token", "DSAJKLE32423JKLCSDJLVI09080");
+  localStorage.setItem("user_id", 232);
+} else {
+  console.log("User id found:", uid);
+  console.log("clearing the user id now...");
+  localStorage.clear();
+}
