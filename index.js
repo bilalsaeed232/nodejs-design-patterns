6a73f15197e0751be77e812629f72ff76c8ffbbe
Bilal Saeed <bilalsaeed232@gmail.com>
2021-03-22 17:53:58 +0100

    THE PROBLEM: in order to have proper separation of concerns, we need an action objects which contains the required data for the execution of that action and all this is separate from action executor

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-//entry point file...
+const { createInterface } = require("readline");
+
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+console.log("create <fileName> <text> | exit");
+rl.prompt();
+
+rl.on("line", (input) => {
+  const [commandText, ...remainingText] = input.split(" ");
+  const [fileName, ...fileText] = remainingText;
+  const text = fileText.join(" ");
+
+  switch (commandText) {
+    case "exit":
+      console.log("TODO: exit");
+      break;
+
+    case "create":
+      console.log("TODO: create file!");
+      console.log("File content:", text);
+      break;
+
+    default:
+      console.log(`Invalid command ${commandText}`);
+      break;
+  }
+
+  rl.prompt();
+});
